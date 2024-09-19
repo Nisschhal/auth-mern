c3a392d5eed765098daa2cb0ee3854362f7a116a
Nisschhal <mrnischalpuri@gmail.com>
2024-09-19 15:37:26 +0545

    signup UI created: installed lucide-react for icons, react-router-dom for routes and custom signup UI with framer-motion for animation

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,6 +1,82 @@
+import { motion } from "framer-motion";
+import { User, Mail, Lock } from "lucide-react";
+import Input from "../components/Input";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const handleSignup = (e) => {
+    e.preventDefault();
+  };
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <motion.div
+      className={`max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl`}
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <div className="p-8">
+        <h1
+          className={`mb-6 bg-gradient-to-r from-green-300 to-emerald-400 text-3xl font-bold text-center text-transparent bg-clip-text`}
+        >
+          Create Account
+        </h1>
+
+        {/* // SIGN UP FORM */}
+        <form onSubmit={handleSignup}>
+          {/* INPUT: FULLNAME */}
+          <Input
+            icon={User}
+            type="text"
+            placeholder="Full Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          {/* INPUT: EMAIL */}
+          <Input
+            icon={Mail}
+            type="text"
+            placeholder="Email Address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {/* INPUT: FULLNAME */}
+          <Input
+            icon={Lock}
+            type="text"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          {/* Password strength meter */}
+
+          {/* Submit button */}
+          <motion.button
+            className="w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
+            whileHover={{ scale: 1.02 }}
+            whileTap={{ scale: 0.98 }}
+            type="submit"
+          >
+            Sign up
+          </motion.button>
+        </form>
+      </div>
+
+      {/* Already have an account */}
+      <div className="px-8 py-4 flex justify-center bg-gray-900 opacity-50 rounded-b-2xl">
+        <p className="text-sm text-gray-400">
+          Already have an account?{" "}
+          <Link className="text-green-400 hover:underline" to="/login">
+            Login
+          </Link>
+        </p>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Signup;
